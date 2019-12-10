b9d89fd8e30aa606003c5468c845cafe49d89385
zhongjiangbo <zhongjaingbo@qiyi.com>
2019-12-10 16:55:11 +0800

    哈哈哈

--- test.java
+++ test.java
@@ -2,25 +2,3 @@
 
 快速扫描耗时：0·133扫码视频数量：279
 快速支持视频格式：19[·3g2，
-.FLV,.MKV,.MOV,.MPG，
-.avl,
-
-.m2t,
-
-.mp4，
-
-.mlv，
-.mkv，
-·m2v，
-.m4v，
-14197
-14197
-.mkv，
-14197
-14197
-14197
--14338.com/iqiyi.wlanpIay
--14338.com/iqiyi.wlanpIay
-.mp4，.mpg,·qt，
-·ts，
--14338.com
